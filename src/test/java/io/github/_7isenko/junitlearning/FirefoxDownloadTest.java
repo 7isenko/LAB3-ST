8470b6fb9115f8e0eb79c047c22105c404004b3e
7isenko <7isenko.max@gmail.com>
2022-09-27 01:10:59 +0300

    i forgor

--- src/test/java/io/github/_7isenko/junitlearning/FirefoxDownloadTest.java
+++ src/test/java/io/github/_7isenko/junitlearning/FirefoxDownloadTest.java
@@ -1,7 +1,9 @@
 package io.github._7isenko.junitlearning;
 
+import io.github._7isenko.junitlearning.pages.DownloadPage;
 import io.github._7isenko.junitlearning.utils.Properties;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -9,11 +11,40 @@ import java.time.Duration;
 /**
  * @author 7isenko
  */
-public class FirefoxDownloadTest extends ChromeDownloadTest {
+public class FirefoxDownloadTest {
+
+    protected static final String ACCESSIBLE_LINK = "https://drive.google.com/file/d/1cysw860ctQFtCIMf7mSm3awGRL0mxOSE/view?usp=sharing";
+    protected static final String NOT_ACCESSIBLE_LINK = "https://drive.google.com/file/d/1jNmN2QImBRY_MPBueFPDyybYp3mFYaUs/view?usp=sharing";
+
+    protected static WebDriver driver;
+    protected static DownloadPage downloadPage;
+
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.gecko.driver", Properties.getProperty("geckodriver"));
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
+
+    @Test
+    @DisplayName("File with access should be downloadable")
+    void downloadWithAccess() {
+        downloadPage = new DownloadPage(driver);
+        driver.get(ACCESSIBLE_LINK);
+        Assertions.assertTrue(downloadPage.isDownloadable());
+    }
+
+    @Test @DisplayName("File without access should not be downloadable")
+    void downloadWithNoAccess() {
+        downloadPage = new DownloadPage(driver);
+        driver.get(NOT_ACCESSIBLE_LINK);
+        Assertions.assertFalse(downloadPage.isDownloadable());
+    }
+
+    @AfterAll
+    public static void closeBrowser() {
+        driver.quit();
+    }
+
+
 }
--- src/test/java/io/github/_7isenko/junitlearning/FirefoxDriveTest.java
+++ src/test/java/io/github/_7isenko/junitlearning/FirefoxDriveTest.java
@@ -1,9 +1,11 @@
 package io.github._7isenko.junitlearning;
 
+import io.github._7isenko.junitlearning.pages.DownloadPage;
 import io.github._7isenko.junitlearning.pages.DriveAccessIFrame;
 import io.github._7isenko.junitlearning.pages.DrivePage;
 import io.github._7isenko.junitlearning.utils.Properties;
-import org.junit.jupiter.api.BeforeAll;
+import io.github._7isenko.junitlearning.utils.UserInputUtils;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -15,7 +17,13 @@ import java.time.Duration;
 /**
  * @author 7isenko
  */
-public class FirefoxDriveTest extends ChromeDriveTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FirefoxDriveTest {
+
+    protected static DrivePage drivePage;
+    protected static DriveAccessIFrame driveAccessIFrame;
+    protected static WebDriver driver;
+
     @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.gecko.driver", Properties.getProperty("geckodriver"));
@@ -31,7 +39,105 @@ public class FirefoxDriveTest extends ChromeDriveTest {
         driver.get(Properties.getProperty("drivepage"));
     }
 
-    @Override
+
+    @Test
+    @Order(1)
+    @DisplayName("Should upload file")
+    void fileUploadTest() {
+        drivePage.openMenu();
+        drivePage.uploadButtonClick();
+        drivePage.uploadToFileInput(Properties.getProperty("uploadfilepath") + Properties.getProperty("uploadfilename"));
+        Assertions.assertTrue(drivePage.isUploadedFileAccessible());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Should change file access to \"by link\"")
+    void accessTest() {
+        drivePage.openUploadedFileContextMenu();
+        drivePage.pressAccessOnUploadedFileMenu();
+        driveAccessIFrame.switchToIFrame();
+        driveAccessIFrame.openAccessMenu();
+        driveAccessIFrame.clickAccessWithLink();
+        Assertions.assertTrue(driveAccessIFrame.isAccessGiven());
+        driveAccessIFrame.copyLink();
+        driveAccessIFrame.closeMenu();
+        drivePage.switchToDefaultContent();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Link should lead to downloadable file by guest")
+    void accessLinkTest() {
+        WebDriver driverNoProfile = getDriverNoProfile();
+        DownloadPage downloadPage = new DownloadPage(driverNoProfile);
+        driverNoProfile.get(UserInputUtils.getClipboard());
+
+        Assertions.assertTrue(downloadPage.isDownloadable());
+        driverNoProfile.quit();
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Should create directory")
+    void createDirectoryTest() {
+        drivePage.openMenu();
+        drivePage.newDirectoryButtonClick();
+        drivePage.fillNewDirectoryInput();
+        Assertions.assertTrue(drivePage.isDirectoryAccessible());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Should drag and drop file, so file must be moved")
+    void dragAndDropFileTest() {
+        drivePage.dragAndDropFile();
+        Assertions.assertFalse(drivePage.isUploadedFileAccessible());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Should drag and drop file back, so directory is empty")
+    void moveFileBackTest() {
+        drivePage.moveToDirectory();
+        drivePage.moveFileBack();
+        Assertions.assertTrue(drivePage.isDirectoryEmpty());
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Should encounter file on main page")
+    void fileIsAvailableInMainPage() {
+        drivePage.moveToMainPage();
+        Assertions.assertTrue(drivePage.isUploadedFileAccessible());
+    }
+
+
+    @Test
+    @Order(8)
+    @DisplayName("Should delete file")
+    void deleteFileTest() {
+        drivePage.openUploadedFileContextMenu();
+        drivePage.pressDeleteOnContextMenu();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        Assertions.assertFalse(drivePage.isUploadedFileAccessible());
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Should delete directory")
+    void deleteDirectoryTest() {
+        drivePage.openDirectoryContextMenu();
+        drivePage.pressDeleteOnContextMenu();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        Assertions.assertFalse(drivePage.isDirectoryAccessible());
+    }
+
+    @AfterAll
+    public static void finish() {
+        driver.quit();
+    }
+
     protected WebDriver getDriverNoProfile() {
         System.setProperty("webdriver.gecko.driver", Properties.getProperty("geckodriver"));
         WebDriver driverNoProfile = new FirefoxDriver();
